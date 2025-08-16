622b3c5c80e5567cc9049d3aec9ab1c6a4d9f37a
davidramir <emilio.ramirez@uab.edu.bo>
2025-08-15 22:48:47 -0400

    login con Sanctum

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -4,7 +4,12 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\IngresoController;
 use App\Http\Controllers\ProductoController;
+use App\Http\Controllers\AuthController;
 
 // Rutas para API REST
 Route::apiResource('ingresos', IngresoController::class);
 Route::apiResource('productos', ProductoController::class);
+
+Route::post('/register', [AuthController::class, 'register']);
+
+Route::post('/login', [AuthController::class, 'login']);
